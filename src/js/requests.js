364d3ddc27051e5a709fc115caeb93e3f426f033
Roman Telik <122562266+telezhka@users.noreply.github.com>
2023-06-28 16:28:17 +0300

    Update requests.js

--- src/js/requests.js
+++ src/js/requests.js
@@ -1,9 +1,9 @@
-const URL = 'https://pixabay.com/api/';
-const API_KEY = '37812301-bb78e35e415e6149d67a423b2';
+const url = 'https://pixabay.com/api/';
+const api_key = '37812301-bb78e35e415e6149d67a423b2';
 
 const fetchSearch = async (requestPic, page) => {
   const response = await fetch(
-    `https://pixabay.com/api/?key=37812301-bb78e35e415e6149d67a423b2&q=${requestPic}&image_type=photo&orientation=horizontal&per_page=40&page=${page}`
+     `${url}?key=${api_key}&q=${requestPic}&image_type=photo&orientation=horizontal&per_page=40&page=${page}`
   );
   const users = await response.json();
   return users;
