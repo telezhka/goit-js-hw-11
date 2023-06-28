2b0c6e6657027846f830a14d2e12aa31ef3ee8aa
Roman Telik <rtelik58@gmail.com>
2023-06-28 22:24:31 +0300

    redone

--- src/js/requests.js
+++ src/js/requests.js
@@ -3,7 +3,7 @@ const api_key = '37812301-bb78e35e415e6149d67a423b2';
 
 const fetchSearch = async (requestPic, page) => {
   const response = await fetch(
-     `${url}?key=${api_key}&q=${requestPic}&image_type=photo&orientation=horizontal&per_page=40&page=${page}`
+    `${url}?key=${api_key}&q=${requestPic}&image_type=photo&orientation=horizontal&per_page=40&page=${page}`
   );
   const users = await response.json();
   return users;
